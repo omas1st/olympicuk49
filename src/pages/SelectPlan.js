a8ad549758abf87f6d598bd8a151b8209e6d1a8c
omas1st <omas1st@gmail.com>
2025-12-12 20:45:00 +0100

    Update payment reference number in SelectPlan.js

--- src/pages/SelectPlan.js
+++ src/pages/SelectPlan.js
@@ -51,7 +51,7 @@ export default function SelectPlan() {
           Account: 62509963139<br/>
           Reference: 0651623286<br/>
           FNB (250655)<br/>
-          Note: Always include "0631093130" as the reference number when making payment,
+          Note: Always include "0651623286" as the reference number when making payment,
           your payment won't be processed, if you fail to add the reference number.<br/>
             Send receipt via WhatsApp to +1 405 926 0437 or +44 739 887 1333.<br/>
           Immediate Payment.
